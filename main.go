85f3c4e0ac9cf2d5b967ca897ef2251b43cc0202
Devata Dinesh Vamsi Durga Bhaskar <99879261+devataDinesh@users.noreply.github.com>
2022-08-18 17:52:50 +0530

    Updated getListhandler()

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"github.com/fatih/color"
 	"net/http"
+	"project/cache"
+	"project/helper"
 )
+
+var list = cache.WebsiteList
 
 func main() {
 
@@ -20,7 +25,21 @@ func main() {
 }
 
 func getListHandler(w http.ResponseWriter, r *http.Request) {
+	var err error
+	list, err = helper.GetList(r)
 
+	if err != nil {
+		color.Red("\t400 BAD REQUEST\n...........Failed to retrieve the WebsiteList :(...........")
+	} else {
+		fmt.Fprint(w, list)
+
+		color.Green("\t200 OK\n...........Successfully retrieved the WebsiteList :)...........")
+		color.Cyan("The received List is : ")
+
+		for index, _ := range list {
+			fmt.Println("\t", index)
+		}
+	}
 }
 
 func checkStatusHandler(w http.ResponseWriter, r *http.Request) {
